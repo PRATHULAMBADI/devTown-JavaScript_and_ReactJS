95a428f011ac863a3334bc1542f8bb60fea4ca77
Prathul V <44966653+PRATHULAMBADI@users.noreply.github.com>
2023-04-17 21:18:58 +0530

    first commit
    Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Notes from "./components/Notes";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
